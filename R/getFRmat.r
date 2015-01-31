f6a2e080f1bb7808cb7fe8f450807d840b88307d
Hsiao Chiaowen Joyce <joyce.hsiao1@gmail.com>
2015-01-31 04:25:01 +0000

    setting up git-svn bridge
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_0/madman/Rpacks/flowMap@98926 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getFRmat.r
+++ R/getFRmat.r
@@ -1,38 +1,44 @@
-#' F-R tests for two-sampbetween two samples
+#' FR tests to compare two flow cytometry samples 
 #'
-#' Compute the F-R statistics for all possible population comparisons between two samples. 
-#' The function also provides an argument iiTest that selects a subset of population comparions
-#' that are likely to produce a match. The computing time of this function grows with sampleSize
-#' and the number of population comparions.
+#' Perform FR tests to compare cell populations across two flow cytometry samples.
+#' The FR statistics are estimated based on a downsampling scheme designed to optimize runtime as well as
+#' precision and accuracy of the statistics. The downsampling scheme samples from the pooled data of 
+#' events across a single cell population comparison. The events in the sample maintain the same cell 
+#' population membership ratio as the events in the pooled data (\emph{proportional}) or include an
+#' equal number of events from the two cell populations in the comparison.
 #'  
-#' @param i dummy variable to initialize parallel computing (package Rparallel)
-#' @param XX1 matrix or data.frame of events by feature where the events are indexed by a variable id in the 
-#'            last column of the matrix or data.frame. This sample is consisted of the populations to be mapped.
-#' @param XX2 matrix or data.frame of events by feature where the events are indexed by a variable id in the 
-#'            last column of the matrix or data.frame. This sample is the designated reference sample. 
-#'            XX1 populations are mapped onto the XX2 populations during cross-sample comparions.
-#' @param iiTest matrix of 0/1, indexing the population pairs across the two samples that are 
-#'              likely to produce a match. Default is all pairs. The first column of iiTest indexes the 
-#'              population ID in XX1 and the subsequent columns index the XX2 populations to be compared 
-#'              with each XX1 population.
-#' @param sampleMethod method of downsampling. Currently, the only available method is equalSize. This method
-#'                    randomly selects the same number of events from each group. 
-#' @param sampleSize given the method equalSize, specify the number of events to sample in each group 
-#'                  of a population comparison.
+#' @param XX1 a flow cytometry sample of cell populations, organized in a matrix or a data.frame of events (rows)
+#'            by features (columns) where cell populaiton memberships are indexed in the last column by 
+#'            a variable named id.
+#' @param XX2 a flow cytometry sample of cell populations, organized in a matrix or a data.frame of events (rows)
+#'            by features (columns) where cell populaiton memberships are indexed in the last column by 
+#'            a variable named id.
+#' @param sampleMethod downsampling method, options include \emph{equalSize} or \emph{proportional}. Both methods 
+#'                    sample events without replacement from the combined events in a single cell population pair 
+#'                    comparison. Using \emph{equalSize}, each sample includes an equal number of events from the
+#'                    two cell populations being compared. Using \emph{proportional}, the ratio of the event 
+#'                    membership is same as the ratio of event membershiop prior to sampling. 
+#'                    (default: \emph{proportional})
+#' @param sampleSize specifies \emph{S},the number of events to be included in each sample. For \emph{equalSize} sampling 
+#'					, S/2 is sampled from each population. For \emph{proportional} sampling, the ratio of event membership
+#'					is the same as the ratio of event membership prior to sampling.
+#' @param i dummy variable to initialize parallel computing (\pkg{doParallel}).
 #' 
-#' @return wmat FR statistics for each population comparison (XX1 populations by XX2 populations). 
-#'              The table entry is marked as N/A if F-R test is not performed per filtering table iiTest.
-#' @return runsmat the number of runs for each comparison (XX1 populations by XX2 populations)
-#' @return mumat the expected number of runs for each comparison (XX1 populations by XX2 populations)
-#' @return sigma2mat the estimated variance of runs for each comparison (XX1 populations by XX2 populations)
-#' @return pNormat the two-sided p-value associated with the F-R statistic under asymptotic normal assumption 
-#'                  of the F-R statistic (XX1 populations by XX2 populations)
+#' @return wmat a list of matrices containing sample FR statistics for each XX1 by XX2 population comparisons.
+#' @return runsmat a list of matrices containing sample runs for each XX1 by XX2 population comparisons.
+#' @return mumat a list of matrices containing sample expected number of runs for each XX1 by XX2 population comparisons.
+#' @return sigma2mat a list of matrices containing sample estimated variance of runs for each XX1 by XX2 population comparisons.
+#' @return pNormat a list of matrices containing sample one-sided p-value associated with the FR statistics of 
+#'                XX1 by XX2 population comparisons under the asymptotic normality assumption of the FR statistic.
 #'
 #' @name getFRmat
 #' 
+#' @author Chiaowen Joyce Hsiao \email{joyce.hsiao1@@gmail.com}
+#' 
 #' @export
-getFRmat <- function(i,XX1,XX2,iiTest=NULL,sampleMethod,sampleSize) { 
-    XX1 <- as.data.frame(XX1)
+getFRmat <- function(XX1,XX2,sampleMethod,sampleSize,i=NULL) { 
+
+  	XX1 <- as.data.frame(XX1)
     XX2 <- as.data.frame(XX2)
 
     XX1.list <-split(XX1,f=XX1$id)
@@ -44,13 +50,13 @@ getFRmat <- function(i,XX1,XX2,iiTest=NULL,sampleMethod,sampleSize) {
     F1 <- dim(XX1.list[[1]])[2]-1
     F2 <- dim(XX2.list[[1]])[2]-1 
 
-    XX1.list <- lapply(XX1.list,function(x) {
-      x <- x[,1:F1]
-      return(x)})
+    XX1.list <- lapply(XX1.list,function(xx) {
+      xx <- xx[,1:F1]
+      return(xx)})
 
-    XX2.list <- lapply(XX2.list,function(x) {
-      x <- x[,1:F2]
-      return(x)})
+    XX2.list <- lapply(XX2.list,function(xx) {
+      xx <- xx[,1:F2]
+      return(xx)})
 
 
     wmat <- matrix(NA,lvl1,lvl2)
@@ -62,22 +68,21 @@ getFRmat <- function(i,XX1,XX2,iiTest=NULL,sampleMethod,sampleSize) {
     runsmat <- wmat
     pNormat <- wmat
 
-    rep <- i
-
-    if (is.null(iiTest)) {
-        sps <- length(unique(XX1$id))
-        ref.groups <- sort(unique(XX2$id))
-        temp <- matrix(ref.groups,ncol=length(ref.groups),nrow=sps,byrow=T)
-        iiTest <- data.frame(sp=sort(unique(XX1$id)),temp)
-    }
+    # set up index array
+    sps <- length(unique(XX1$id))
+    ref.groups <- sort(unique(XX2$id))
+    temp <- matrix(ref.groups,ncol=length(ref.groups),nrow=sps,byrow=T)
+    iiTest <- data.frame(sp=sort(unique(XX1$id)),temp)
 
     testPops <- as.numeric(as.character(iiTest[,1]))
 
     for (jj in seq_along(testPops)) {
-      refPops <- iiTest[jj, c(2:ncol(iiTest)) ]
+
+	  refPops <- iiTest[jj, c(2:ncol(iiTest)) ]
 
       for (kk in seq_along(refPops)) {
-        j <- testPops[jj]
+
+		j <- testPops[jj]
         k <- as.numeric(as.character( refPops )[kk])
 
         iirow <- which(rownames(wmat)==j)
@@ -88,23 +93,40 @@ getFRmat <- function(i,XX1,XX2,iiTest=NULL,sampleMethod,sampleSize) {
 
         if (sampleMethod=="equalSize") {
 
-          if (nrow(xx1) > sampleSize) {
-           iisam1 <- sample(nrow(xx1),sampleSize)
+		   groupSize <- sampleSize/2
+
+		   if (nrow(xx1) > groupSize) {
+           iisam1 <- sample(nrow(xx1),groupSize)
           } else {
-           iisam1 <- c(1:nrow(xx1))
+		   iisam1 <- sample(nrow(xx1),groupSize,replace=TRUE)
           }
-          if (nrow(xx2) > sampleSize) {
-           iisam2 <- sample(nrow(xx2),sampleSize)
+          if (nrow(xx2) > groupSize) {
+           iisam2 <- sample(nrow(xx2),groupSize)
           } else {
-           iisam2 <- c(1:nrow(xx2))
+           # iisam2 <- c(1:nrow(xx2))
+           iisam2 <- sample(nrow(xx2),groupSize,replace=TRUE)
           }          
-        } else  {
+         mat1 <- xx1[iisam1,]
+         mat2 <- xx2[iisam2,]
+        } else  if (sampleMethod=="proportional") {
+		   prop_temp <- round(nrow(xx1)/(nrow(xx1)+nrow(xx2)),2)
+		   if (prop_temp==1) {prop_take=0.99}
+		   if (prop_temp<1) {prop_take=prop_temp}
+		   n1 <- sampleSize*prop_take
+           n2 <- sampleSize-n1
+           iisam1 <- sample(nrow(xx1),n1,replace=TRUE)
+           iisam2 <- sample(nrow(xx2),n2,replace=TRUE) 
+           mat1 <- xx1[iisam1,]
+           mat2 <- xx2[iisam2,]
+        } else if (sampleMethod=="none") {
+            mat1 <- xx1
+            mat2 <- xx2
+        } else {
           message("warning: sampling method unspecified")
         }
-        mat1 <- xx1[iisam1,]
-        mat2 <- xx2[iisam2,]
 
         temp <- getFR(mat1,mat2)
+
         wmat[iirow,iicol] <- temp$ww
         mumat[iirow,iicol] <- temp$mu
         sigma2mat[iirow,iicol] <- temp$sigma2
